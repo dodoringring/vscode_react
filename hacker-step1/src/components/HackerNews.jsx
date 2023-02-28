dc29586df621a2da250220cecfeaa081191d0454
dodoringring <dodohee1504@gmail.com>
2023-02-28 17:23:13 +0900

    02/28 수업정리

--- hacker-step1/src/components/HackerNews.jsx
+++ hacker-step1/src/components/HackerNews.jsx
@@ -1,7 +1,7 @@
-const HackerNews = (props) => {
+const HackerNews = (props) => { //{newsList:Array(30)}원래 이렇게 되어 있었다. 기존의 자바스크립트보다 훨씬 더 데이터 셋과의 연계가 자유롭다.}
   const {newsList}=props;
-  /* console.log(props);
-  console.log(props.newsList);->구조분해할당이 된다. */
+  console.log(props);
+  // console.log(props.newsList);//->구조분해할당이 된다.
   return (  
     <div>
       {/* [{},{}...] 맵으로 꺼낸다. */}
